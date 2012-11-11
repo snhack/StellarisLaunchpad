d0b3b66b320d3d2d1149323a0b3af34f44462021
Tony Walmsley <tonywalmsley@gmail.com>
2012-11-11 14:43:18 +0000

    Changed the messages.

--- LedArrayDriver/main.cpp
+++ LedArrayDriver/main.cpp
@@ -53,20 +53,43 @@ int main(void) {
 	LedArrayDriver led(&clock, &chipSelect, &shift, &a,&b,&c, NBR_OF_DISPLAY_ROWS, NBR_OF_DISPLAY_COLUMNS);
 	led.init();
 
-	AsciiMessage welcome("**** Welcome to the Swindon Hackspace at the Museum of Computing - Wednesdays 6:30pm to 10pm ****");
+	AsciiMessage welcome("**** Welcome to the Museum of Computing - open Fridays 10am to 4pm and Saturdays 9:30am to 5pm ****");
+	AsciiMessage exhib1("**** Visit our \"Gaming on the Go\" exhibition - 35 years of handheld games consoles ****");
+	AsciiMessage retroGaming("**** Join our computer games sessions on Saturdays for some retro gaming ****");
+	AsciiMessage website("**** See us on the web at http://www.museum-of-computing.org.uk/ ****");
+	AsciiMessage hackspace("**** Join the Swindon Hackspace at the Museum of Computing - Wednesdays 6:30pm to 10pm ****");
+	AsciiMessage compClub("**** Ask about our Kids Computer Club on Saturdays ****");
+	AsciiMessage schoolVisit("**** Arrange a school visit and take part in a tour of the museum, quizzes and mini projects ****");
+	AsciiMessage shop("**** Our shop has a range of gifts and souvenirs ****");
+
 	RepeatedGraphic multiInvader(invader, 10, 8);
 	AnimatedGraphic pacman(animPacman, 17, 7);// columns * frames
 	AnimatedGraphic invader(animInvader, 11, 4);// columns * frames
 
+
 	LeftScroller scrollLeft1(led, invader, 2);
-	LeftScroller scrollLeft2(led, multiInvader, 1);
-	LeftScroller scrollLeft3(led, pacman, 1);
-	LeftScroller scrollLeft4(led, welcome, 1);
+	LeftScroller scrollLeft2(led, welcome, 1);
+	LeftScroller scrollLeft3(led, exhib1, 1);
+	LeftScroller scrollLeft4(led, multiInvader, 1);
+	LeftScroller scrollLeft5(led, retroGaming, 1);
+	LeftScroller scrollLeft6(led, pacman, 1);
+	LeftScroller scrollLeft7(led, compClub, 1);
+	LeftScroller scrollLeft8(led, schoolVisit, 1);
+	LeftScroller scrollLeft9(led, website, 1);
+	LeftScroller scrollLeft10(led, shop, 1);
+	LeftScroller scrollLeft11(led, hackspace, 1);
 	do
 	{
 		while(scrollLeft1.animate());
 		while(scrollLeft2.animate());
 		while(scrollLeft3.animate());
 		while(scrollLeft4.animate());
+		while(scrollLeft5.animate());
+		while(scrollLeft6.animate());
+		while(scrollLeft7.animate());
+		while(scrollLeft8.animate());
+		while(scrollLeft9.animate());
+		while(scrollLeft10.animate());
+		while(scrollLeft11.animate());
 	} while(1);
 }
